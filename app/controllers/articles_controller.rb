b6b10715e6c4eb77a7055984dd754c22685c539c
Tellarra <richaradele@gmail.com>
2022-02-09 15:17:57 +0100

    Add articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @list_article = Article.all
+    end
 end
