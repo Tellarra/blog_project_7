60c7dbaa4397b5850f5741ef4bcf62358a63f723
Tellarra <richaradele@gmail.com>
2022-02-09 16:00:32 +0100

    Render plain working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
     def index
         @list_article = Article.all
     end
+
+    def new
+
+    end
+
+    def create
+        @new_article = Article.new(params.require(:article).permit(:title, :description))
+        render plain: @new_article.inspect
+    end
 end
